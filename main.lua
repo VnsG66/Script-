7b1a59317cf9ca550b92addb2eff2bd8ba69f410
VnsG66 <ninenplays@gmail.com>
2025-08-14 13:45:15 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,12 +1,10 @@
 -- main.lua
-
 local get = function(url)
     local fn, err = loadstring(game:HttpGet(url))
     if not fn then error(err) end
     return fn()
 end
 
--- Raw URLs for your modules (make sure these are correct and public)
 local ShopList = get("https://raw.githubusercontent.com/VnsG66/Script-/main/shoplist.lua")
 local Functions = get("https://raw.githubusercontent.com/VnsG66/Script-/main/functions.lua")
 local createGui = get("https://raw.githubusercontent.com/VnsG66/Script-/main/gui.lua")
@@ -19,10 +17,8 @@ local ShopRemotes = {
 }
 
 _G.AutoBuyToggles = _G.AutoBuyToggles or {Seeds = false, Gears = false}
+_G.SelectedItems = _G.SelectedItems or {Seeds = {All=false}, Gears = {All=false}}
 
 Functions.cleanupGui()
-
-local closeGuiFunc = createGui(_G.AutoBuyToggles)
-Functions.startAutoBuy(ShopRemotes, ShopList, _G.AutoBuyToggles)
-
--- Optionally: You can call closeGuiFunc() to close the GUI via script later if needed
+createGui(ShopList, _G.AutoBuyToggles, _G.SelectedItems)
+Functions.startAutoLoop(ShopRemotes, ShopList, _G.AutoBuyToggles, _G.SelectedItems)
