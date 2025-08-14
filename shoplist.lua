b56303e41422bb114199168bc76d82716afba674
VnsG66 <ninenplays@gmail.com>
2025-08-14 13:06:29 +0800

    Update shoplist.lua

--- shoplist.lua
+++ shoplist.lua
@@ -1,1 +1,16 @@
-shoplist
+-- ShopList.lua
+return {
+    Seeds = {
+        "Carrot", "Strawberry", "Blueberry", "Orange Tulip", "Tomato", "Corn", "Daffodil", "Watermelon", "Pumpkin",
+        "Apple", "Bamboo", "Coconut", "Cactus", "Dragon Fruit", "Mango", "Grape", "Mushroom", "Pepper", "Cacao",
+        "Beanstalk", "Ember Lily", "Sugar Apple", "Burning Bud", "Giant Pinecone", "Elder Strawberry"
+    },
+    Gears = {
+        "Watering Can", "Trading Ticket", "Trowel", "Recall Wrench", "Basic Sprinkler", "Advanced Sprinkler", "Medium Toy",
+        "Medium Treat", "Godly Sprinkler", "Magnifying Glass", "Master Sprinkler", "Cleaning Spray", "Favorite Tool",
+        "Harvest Tool", "Friendship Pot", "Grandmaster Sprinkler", "Levelup Lollipop"
+    },
+    Eggs = {
+        "Common", "Common Summer", "Rare Summer", "Mythical", "Paradise", "Bug"
+    }
+}
