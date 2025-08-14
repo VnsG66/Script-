cf56e0f85d6438a156c2c09ffaef44365163947b
VnsG66 <ninenplays@gmail.com>
2025-08-14 13:09:42 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,13 +1,10 @@
 -- main.lua
 
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local Players = game:GetService("Players")
-
 local ShopList = require(script.ShopList)
 local Functions = require(script.Functions)
 local createGui = require(script.Gui)
 
--- Setup remotes table
 local GameEvents = ReplicatedStorage:WaitForChild("GameEvents")
 local ShopRemotes = {
     Seeds = GameEvents:FindFirstChild("BuySeedStock") or GameEvents:WaitForChild("BuySeedStock", 5),
@@ -18,6 +15,7 @@ _G.AutoBuyToggles = _G.AutoBuyToggles or {Seeds = false, Gears = false}
 
 Functions.cleanupGui()
 
-createGui(_G.AutoBuyToggles)
-
+local closeGuiFunc = createGui(_G.AutoBuyToggles)
 Functions.startAutoBuy(ShopRemotes, ShopList, _G.AutoBuyToggles)
+
+-- Optionally, you can call closeGuiFunc() to programmatically close the GUI from elsewhere.
